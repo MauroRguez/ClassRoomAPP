a69eca9eff57809aba4dfec88ea830bfb52299d6
MauroRguez <mrodriguez1214@hotmail.com>
2025-03-07 10:23:16 -0500

    creando tablas

---
+++ src/main/java/com/example/APIClassRoom/modelos/Estudiante.java
@@ -0,0 +1,52 @@
+package com.example.APIClassRoom.modelos;
+
+import java.time.LocalDate;
+
+public class Estudiante {
+    private Integer id;
+    private Integer grado;
+    private LocalDate fechaNacimiento;
+    private String direccion;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(Integer id, Integer grado, LocalDate fechaNacimiento, String direccion) {
+        this.id = id;
+        this.grado = grado;
+        this.fechaNacimiento = fechaNacimiento;
+        this.direccion = direccion;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getGrado() {
+        return grado;
+    }
+
+    public void setGrado(Integer grado) {
+        this.grado = grado;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
